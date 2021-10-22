44cd4e8bff976c8e519357b9fc62e998b70ca7a8
abhiraj <thakurabhiraj4@gmail.com>
2021-10-23 01:20:33 +0530

    alpha1

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,28 @@ import { GraphQLServer } from 'graphql-yoga'
 import { createContext } from './context'
 import { permissions } from './permissions'
 import { schema } from './schema'
+import express from 'express'
+const path = require('path')
 
 const port = process.env.PORT || 4000
 
-new GraphQLServer({
+const server = new GraphQLServer({
   schema,
   context: createContext,
   middlewares: [permissions],
-}).start({ port: port }, () =>
-  console.log(`🚀 Server ready at: http://localhost:${port}`),
-)
+})
+
+const buildPath = path.normalize(path.join(__dirname, '../../web/build'));
+
+server.express.use('/uploads', express.static(path.join(buildPath)));
+server.express.get('/build', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../../web/build/index.html'))
+})
+server.start({port}, () => console.log(`Server is running on localhost:${port}`))
+// // Serve static files from the React frontend app
+// app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+// // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+// })
