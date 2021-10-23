b32f8bf9d16e8bb88ff536e5b91909a69d66ff78
abhiraj <thakurabhiraj4@gmail.com>
2021-10-23 16:59:37 +0530

    mark1

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -4,7 +4,10 @@ import * as types from './types'
 
 export const schema = makeSchema({
   types,
-  plugins: [nexusPrisma()],
+  plugins: [nexusPrisma({
+    // Fixes the Cannot find NexusPrisma issue
+    outputs: { typegen: __dirname + '/generated/index.ts' },
+  })],
   outputs: {
     schema: __dirname + '/../schema.graphql',
     typegen: __dirname + '/generated/nexus.ts',
